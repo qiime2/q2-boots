41bb448a6a5691a2d1aabb1ca65a4b4387b28403
Oddant1 <oddant1@hotmail.com>
2026-02-11 16:57:15 -0700

    Seed rng consistently without making every table identical

--- q2_boots/_resample.py
+++ q2_boots/_resample.py
@@ -5,16 +5,25 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+import random
+
+# Numpy recommends using at least 128 bits of entropy to seed its rng which is
+# where this is gonna end up. This won't quite shake out like that given we are
+# seeding Python's rng with something else but we are gonna try our best.
+MIN_RECOMMENDED_NP_RNG_BITS = 128
+
 
 def resample(ctx, table, sampling_depth, n, replacement, random_seed=None):
     rarefy_action = ctx.get_action('feature_table', 'rarefy')
     resampled_tables = []
 
-    for i in range(n):
+    random.seed(random_seed)
+    for _ in range(n):
+        _random_seed = random.randrange(2**MIN_RECOMMENDED_NP_RNG_BITS)
         resampled_table, = rarefy_action(table=table,
                                          sampling_depth=sampling_depth,
                                          with_replacement=replacement,
-                                         random_seed=random_seed)
+                                         random_seed=_random_seed)
         resampled_tables.append(resampled_table)
 
     return {f'resampled-table-{i}': t for i, t in enumerate(resampled_tables)}
