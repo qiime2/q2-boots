fe3189567578fa10e940d3dbc85b6b7a3f40a351
Oddant1 <oddant1@hotmail.com>
2026-07-01 11:27:05 -0700

    Get 128 bits

--- q2_boots/_resample.py
+++ q2_boots/_resample.py
@@ -5,10 +5,12 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-from numpy.random import default_rng
+from random import Random
 
 from rachis import Artifact
-from rachis.plugin import IContext, CaptureHolder, get_np_random_seed
+from rachis.plugin import (
+    IContext, CaptureHolder, get_np_random_seed, NP_RNG_BITS
+)
 
 
 def resample(ctx: IContext,
@@ -22,9 +24,8 @@ def resample(ctx: IContext,
 
     resampled_tables = []
     random_int = CaptureHolder.get_or_set(random_seed, get_np_random_seed)
-    rng = default_rng(random_int)
-    # The high here is unfortunately capped at 2**64 not 2**128
-    random_seeds = rng.integers(low=0, high=2**64, size=n)
+    rng = Random(random_int)
+    random_seeds = [rng.getrandbits(NP_RNG_BITS) for _ in range(n)]
     for _random_seed in random_seeds:
         resampled_table, = rarefy_action(table=table,
                                          sampling_depth=sampling_depth,
